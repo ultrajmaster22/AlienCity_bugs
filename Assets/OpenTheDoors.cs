1aec20160af791ccbc74c231d076009b78f4b524
ultrajmaster22 <jmarcosmaster@hotmail.com>
2017-12-29 19:14:07 -0300

    Inicio

---
+++ Assets/OpenTheDoors.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class OpenTheDoors : MonoBehaviour {
+	public Animation anim;
+	public bool Chave=false;
+
+
+
+		void OnTriggerEnter(Collider other) 
+		{
+			if (other.gameObject.CompareTag("Player"))
+			{
+			anim.Play("open");
+			}
+		}
+	void OnTriggerExit(Collider other) 
+	{
+		if (other.gameObject.CompareTag("Player"))
+		{
+			anim.Play("close");
+		}
+	}
+
+	public void PegouChave()
+	{
+		Chave = true;
+	}
+
+
+}
+
+
